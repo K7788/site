cd3e02daea9416527797635b9b74f3c2c75489e2
K7788 <kirya.danchenko@bk.ru>
2022-01-11 14:26:34 +0300

    add css

--- client/src/Header/Header.js
+++ client/src/Header/Header.js
@@ -1,14 +1,29 @@
 import React from 'react'
+import  { Button, Navbar, Container, Link , Nav , NavDropdown, Collapse , Brand , Toggle, Item, Divider , expand , variant , bg}  from  'react-bootstrap' ;
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
 
 export default function Header() {
     return (
-        <div>
-         <ul>
-             <li>1</li>
-             <li>2</li>
-             <li>3</li>
-             <li>4</li>
-         </ul>
-        </div>
+        <Navbar  bg="light" expand="lg">
+  <Container>
+      
+    <Navbar.Brand href="#home">King</Navbar.Brand>
+    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+    <Navbar.Collapse id="basic-navbar-nav">
+      <Nav className="me-auto">
+        <Nav.Link href="#home">Главная</Nav.Link>
+        <Nav.Link href="#link">Работы</Nav.Link>
+        <Nav.Link href="#link">Обо мне</Nav.Link>
+        <Nav.Link href="#link">Проекты</Nav.Link>
+        <Nav.Link href="#link">Контакты</Nav.Link>
+        <Button variant="dark">Dark</Button>{' '}
+        
+      </Nav>
+    </Navbar.Collapse>
+  </Container>
+</Navbar>
     )
 }
+
