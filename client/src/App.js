7f4c3774b1e2d69f7d6a8fee0f0f1a69e0d7ca12
Siarhei Mlynchyk <s.mlynchyk@godeltech.com>
2022-01-11 15:52:46 +0300

    add rename

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
-import './Header/Header.css';
-import Header from './Header/Header.js';
-import Footer from './Footer/Footer.js';
-
+import logo from "./logo.svg";
+import "./App.css";
+import "./Header/Header.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Header from "./Header/Header.js";
+import Footer from "./Footer/Footer.js";
 
 function App() {
-  return (
-    
-    <Header />
-    
-  );
+  return <Header />;
 }
 
 export default App;
--- client/src/Header/Header.js
+++ client/src/Header/Header.js
@@ -1,29 +1,25 @@
-import React from 'react'
-import  { Button, Navbar, Container, Link , Nav , NavDropdown, Collapse , Brand , Toggle, Item, Divider , expand , variant , bg}  from  'react-bootstrap' ;
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React from "react";
+import { Button, Navbar, Container, Nav } from "react-bootstrap";
 
-
+import "./Header.css";
 
 export default function Header() {
-    return (
-        <Navbar  bg="light" expand="lg">
-  <Container>
-      
-    <Navbar.Brand href="#home">King</Navbar.Brand>
-    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-    <Navbar.Collapse id="basic-navbar-nav">
-      <Nav className="me-auto">
-        <Nav.Link href="#home">Главная</Nav.Link>
-        <Nav.Link href="#link">Работы</Nav.Link>
-        <Nav.Link href="#link">Обо мне</Nav.Link>
-        <Nav.Link href="#link">Проекты</Nav.Link>
-        <Nav.Link href="#link">Контакты</Nav.Link>
-        <Button variant="dark">Dark</Button>{' '}
-        
-      </Nav>
-    </Navbar.Collapse>
-  </Container>
-</Navbar>
-    )
+  return (
+    <Navbar bg="light" expand="lg">
+      <Container>
+        <Navbar.Brand href="#home">King</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Главная</Nav.Link>
+            <Nav.Link href="#link">Работы</Nav.Link>
+            <Nav.Link href="#link">Обо мне</Nav.Link>
+            <Nav.Link href="#link">Проекты</Nav.Link>
+            <Nav.Link href="#link">Контакты</Nav.Link>
+            <Button variant="dark">Dark</Button>{" "}
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 }
-
