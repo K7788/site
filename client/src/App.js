bddf9f3f1faeab6dbcffab4562f21eeece1309f7
Siarhei Mlynchyk <s.mlynchyk@godeltech.com>
2022-01-11 17:55:16 +0300

    add new pr2

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,10 @@
-import logo from "./logo.svg";
 import "./App.css";
 import "./Header/Header.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Header from "./Header/Header.js";
-import Footer from "./Footer/Footer.js";
+import Home from "./Home/Home";
 
 function App() {
-  return <Header />;
+  return <Home />;
 }
 
 export default App;
---
+++ client/src/HeaderNav/HeaderNav.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { Container } from "react-bootstrap";
+
+import "./HeaderNav.css";
+
+export default function HeaderNav() {
+  return <Container fluid className="container-bg"></Container>;
+}
