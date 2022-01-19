75aa804f8d5115b3218a3b4c5e7c6ae7ac8c8d96
K7788 <kirya.danchenko@bk.ru>
2022-01-19 12:14:29 +0300

    Ne rugai

--- client/src/HeaderNav/HeaderNav.js
+++ client/src/HeaderNav/HeaderNav.js
@@ -1,8 +1,17 @@
 import React from "react";
-import { Container } from "react-bootstrap";
+import { Container, Text } from "react-bootstrap";
 
 import "./HeaderNav.css";
 
 export default function HeaderNav() {
-  return <Container fluid className="container-bg"></Container>;
+  return <Container fluid className="container-bg">
+
+      <p fluid className="text-center">
+            <h1>Я Бурназова Юлия Сергеевна</h1>
+            <h2>Добро пожаловать</h2>
+      </p>
+
+
+  </Container>;
+
 }
