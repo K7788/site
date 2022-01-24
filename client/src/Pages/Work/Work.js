9dc0b84f2b006f0014eaf9b052ec8e3aa7cd9b0d
K7788 <kirya.danchenko@bk.ru>
2022-01-25 00:40:52 +0300

    Lol

---
+++ client/src/Pages/Work/Work.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Container, Row , Col, img} from "react-bootstrap";
+
+import "./Work.css";
+
+export default function Work() {
+  return <>
+
+
+123
+  
+  </>
+
+}
