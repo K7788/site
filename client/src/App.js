a19161a4ac6a84ecc1464e86facd375274e328c3
Siarhei Mlynchyk <s.mlynchyk@godeltech.com>
2022-01-31 10:10:53 +0300

    change struc project)

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,24 @@
-import "./App.css";
-import "./Header/Header.css";
-import "bootstrap/dist/css/bootstrap.min.css";
-import Home from "./Home/Home";
+import React from "react";
+import CoreFeature from "./components/CoreFeature/CoreFeature";
+import Footer from "./components/Footer/Footer";
+import HappyClient from "./components/HappyClient/HappyClient";
+import Header from "./components/Header/Header";
+import HeaderNav from "./components/HeaderNav/HeaderNav";
+import OurProject from "./components/OurProject/OurProject";
+import Services from "./components/Services/Services";
+import Started from "./components/Started/Started";
 
-function App() {
-  return <Home />;
+export default function App() {
+  return (
+    <div>
+      <Header />
+      <HeaderNav />
+      <CoreFeature />
+      <Services />
+      <OurProject />
+      <HappyClient />
+      <Started />
+      <Footer />
+    </div>
+  );
 }
-
-export default App;
