35c0eb3d775247fdc70dd97006191943ebea5935
Siarhei Mlynchyk <s.mlynchyk@godeltech.com>
2022-01-31 16:56:45 +0300

    add link bootstrap

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,36 +1,42 @@
 import React from "react";
-import { Navbar, Nav } from "react-bootstrap";
+import { Navbar, Nav, Col, Container, Row } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
 import "./Header.css";
 
 export default function Header() {
   return (
-    <Navbar bg="light" expand="lg">
-      <div className="container">
-        <p>Бурназова Ю. С.</p>
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <ul>
-              <li>
-                <Link to="/">Главная</Link>
-              </li>
-              <li>
-                <Link to="/work">Работы</Link>
-              </li>
-              <li>
-                <Link to="/about">Обо мне</Link>
-              </li>
-              <li>
-                <Link to="/projects">Проекты</Link>
-              </li>
-              <li>
-                <Link to="/contacts">Контакты</Link>
-              </li>
-            </ul>
-          </Nav>
-        </Navbar.Collapse>
-      </div>
+    <Navbar>
+      <Container fluid>
+        <Row>
+          <Col>
+            <p>Бурназова Ю. С.</p>
+          </Col>
+          <Col>
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="me-auto">
+                <ul>
+                  <li>
+                    <Link to="/">Главная</Link>
+                  </li>
+                  <li>
+                    <Link to="/work">Работы</Link>
+                  </li>
+                  <li>
+                    <Link to="/about">Обо мне</Link>
+                  </li>
+                  <li>
+                    <Link to="/projects">Проекты</Link>
+                  </li>
+                  <li>
+                    <Link to="/contacts">Контакты</Link>
+                  </li>
+                </ul>
+              </Nav>
+            </Navbar.Collapse>
+          </Col>
+        </Row>
+      </Container>
     </Navbar>
   );
 }
