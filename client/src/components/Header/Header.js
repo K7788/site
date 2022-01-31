7557cd397c5e38cb049d926353d45a9407b882a9
Siarhei Mlynchyk <s.mlynchyk@godeltech.com>
2022-01-31 14:39:21 +0300

    rebase pr3

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Navbar, Nav } from "react-bootstrap";
-import { Link, BrowserRouter } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 import "./Header.css";
 
