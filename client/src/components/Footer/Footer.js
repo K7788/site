b546c90d558bfaeaec2d163c64715a5ab1f2b2ff
K7788 <kirya.danchenko@bk.ru>
2022-02-01 15:09:01 +0300

    footee

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -5,81 +5,87 @@ import "./Footer.css";
 export default function Footer() {
   return (
     <>
-      <footer id="fh5co-footer" role="contentinfo">
-        <div className="container">
-          <div className="row row-pb-md">
-            <div className="col-md-4 fh5co-widget">
-              <h3>King.</h3>
-              <p>
-                Facilis ipsum reprehenderit nemo molestias. Aut cum mollitia
-                reprehenderit. Eos cumque dicta adipisci architecto culpa amet.
-              </p>
-              <p>
-                <a href="#">Learn More</a>
-              </p>
-            </div>
-            <div className="col-md-2 col-sm-4 col-xs-6 col-md-push-1">
-              <ul className="fh5co-footer-links">
-                <li>
-                  <a href="#">About</a>
-                </li>
-                <li>
-                  <a href="#">Help</a>
-                </li>
-                <li>
-                  <a href="#">Contact</a>
-                </li>
-                <li>
-                  <a href="#">Terms</a>
-                </li>
-                <li>
-                  <a href="#">Meetups</a>
-                </li>
-              </ul>
-            </div>
+     <footer class="footer">
+<section class="follow">
+<div class="container">
+<h6 class="follow__title visible-xs">
+follow us
+</h6>
+<ul class="follow__list list-unstyled">
+<li class="follow__list__item">
+<a class="btn btn-default btn-default--border btn-full" href="https://www.facebook.com/greetabl" rel="nofollow" target="_blank">
+<span class="visible-xs">
+facebook
+<i class="icon icon-arrows"></i>
+</span>
+<i class="hidden-xs icon icon-facebook"></i>
+</a>
+</li>
+<li class="follow__list__item">
+<a class="btn btn-default btn-default--border btn-full" href="https://twitter.com/greetabl" rel="nofollow" target="_blank">
+<span class="visible-xs">
+twitter
+<i class="icon icon-arrows"></i>
+</span>
+<i class="hidden-xs icon icon-twitter"></i>
+</a>
+</li>
+<li class="follow__list__item">
+<a class="btn btn-default btn-default--border btn-full" href="https://www.pinterest.com/greetabl/" rel="nofollow" target="_blank">
+<span class="visible-xs">
+pinterest
+<i class="icon icon-arrows"></i>
+</span>
+<i class="hidden-xs icon icon-pinterest"></i>
+</a>
+</li>
+<li class="follow__list__item">
+<a class="btn btn-default btn-default--border btn-full" href="https://instagram.com/greetabl/" rel="nofollow" target="_blank">
+<span class="visible-xs">
+instagram
+<i class="icon icon-arrows"></i>
+</span>
+<i class="hidden-xs icon icon-instagram"></i>
+</a>
+</li>
+</ul>
+</div>
+</section>
+<div class="container">
+<ul class="footer__list list-unstyled">
+<li class="footer__list__item">
+<a class="footer__list__item__link" href="/contact">Contact
+</a></li>
+<li class="footer__list__item">
+<a class="footer__list__item__link" rel="nofollow" target="blank" href="https://help.greetabl.com/knowledge">FAQs
+</a></li>
+<li class="footer__list__item">
+<a class="footer__list__item__link" href="/team">Team
+</a></li>
+<li class="footer__list__item">
+<a class="footer__list__item__link" href="/privacy">Privacy
+</a></li>
+<li class="footer__list__item">
+<a class="footer__list__item__link" href="/terms">Terms
+</a></li>
+<li class="footer__list__item">
+<a class="footer__list__item__link" href="https://greetabl.workable.com">Jobs
+</a></li>
+<li class="footer__list__item">
+<a class="footer__list__item__link" href="/reviews">Reviews
+</a></li>
+<li class="footer__list__item">
+<a class="footer__list__item__link" href="/promo-codes">Deals
+</a></li>
+</ul>
+<p class="footer__copyright">
+©
+2022Greetabl.
 
-            <div className="col-md-2 col-sm-4 col-xs-6 col-md-push-1">
-              <ul className="fh5co-footer-links">
-                <li>
-                  <a href="#">Shop</a>
-                </li>
-                <li>
-                  <a href="#">Privacy</a>
-                </li>
-                <li>
-                  <a href="#">Testimonials</a>
-                </li>
-                <li>
-                  <a href="#">Handbook</a>
-                </li>
-                <li>
-                  <a href="#">Held Desk</a>
-                </li>
-              </ul>
-            </div>
-
-            <div className="col-md-2 col-sm-4 col-xs-6 col-md-push-1">
-              <ul className="fh5co-footer-links">
-                <li>
-                  <a href="#">Find Designers</a>
-                </li>
-                <li>
-                  <a href="#">Find Developers</a>
-                </li>
-                <li>
-                  <a href="#">Teams</a>
-                </li>
-                <li>
-                  <a href="#">Advertise</a>
-                </li>
-                <li>
-                  <a href="#">API</a>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </div>
-      </footer>
+All Rights Reserved.
+</p>
+</div>
+</footer>
     </>
   );
 }
